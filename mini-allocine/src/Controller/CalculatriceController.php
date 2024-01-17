4dfcc093c7e4d989c29d17e158d20a3dfdfe32e3
Adel LATIBI <alatibi@campus-eni.fr>
2024-01-17 14:21:01 +0100

    up

--- mini-allocine/src/Controller/CalculatriceController.php
+++ mini-allocine/src/Controller/CalculatriceController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Service\Operations;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CalculatriceController extends AbstractController
 {
     #[Route('/calculatrice', name: 'app_calculatrice')]
-    public function index(): Response
+    public function index(Request $request,Operations $operations): Response
     {
+
+        $op1 = $request->get("op1");
+        $op = $request->get("op");
+        $op2 = $request->get("op2");
+
+        $result = 0;
+        if($op){            
+            /**
+             * function variable
+             * @see https://www.php.net/manual/en/functions.variable-functions.php
+             */
+            $result = $operations->$op($op1,$op2);
+        }
+
         return $this->render('calculatrice/index.html.twig', [
-            'controller_name' => 'CalculatriceController',
+            'result' => $result,
         ]);
     }
 }
